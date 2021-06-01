aeb2e34134e254b5c7b478bea77c93e8851480de
Pavel Korshunov <korsun4@mail.ru>
2021-06-01 22:15:53 +0300

    users table

--- postgresql/create.sql
+++ postgresql/create.sql
@@ -5,3 +5,16 @@ CREATE TABLE orders (
     user_id INTEGER NOT NULL,
     description TEXT
 );
+
+DROP TABLE IF EXISTS users;
+DROP TYPE IF EXISTS active;
+CREATE TYPE active AS ENUM('Y', 'N');
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    active active,
+    login character varying(255) UNIQUE,
+    email character varying(255) UNIQUE,
+    name character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
