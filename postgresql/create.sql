aff8ecec0fe3db2bbe6cc02f8fad23d37a34f126
Pavel <korsun4@mail.ru>
2022-09-02 14:41:42 +0300

    Update create.sql

--- postgresql/create.sql
+++ postgresql/create.sql
@@ -18,3 +18,12 @@ CREATE TABLE users (
     created_at timestamp without time zone,
     updated_at timestamp without time zone
 );
+
+INSERT INTO "orders" ("id", "user_id", "description") VALUES 
+(1, 1, 'Носки'),
+(2, 1, 'Яблоко'),
+(3, 2, 'Яблоко');
+
+INSERT INTO "users" ("id", "active", "login", "email", "name", "created_at", "updated_at") VALUES 
+(1, 'Y', 'admin', 'admin@mail.ru', 'Admin', '2022-08-11 07:58:19', '2022-08-11 07:58:19'),
+(2, 'Y', 'manager', 'manager@mail.ru', 'Manager', '2022-08-11 07:58:19', '2022-08-11 07:58:19');
