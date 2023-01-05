bca29558db39416fb431111d8474038f260850df
Pavel <korsun4@mail.ru>
2023-01-05 19:45:38 +0300

    Create delete.sql

---
+++ postgresql/delete.sql
@@ -0,0 +1,9 @@
+-- Очистка таблиц
+-- Удалит все записи из таблицы orders
+DELETE FROM orders;
+
+-- Удалит все записи из таблицы orders
+TRUNCATE TABLE orders;
+
+-- Удалит запись из таблицы orders, где id = 1
+DELETE FROM orders WHERE id = 1;
